2e82c631e1e404a9c9a6bec8f639ae89c9bc8ee8
Pavel <ozareniep@gmail.com>
2025-03-04 00:12:39 +0300

    v1

---
+++ src/components/shared/products-group-list.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Title } from './title';
+import { ProductCard } from './product-card';
+
+interface Props {
+  title: string;
+  items: any[];
+  className?: string;
+  categoryId: number;
+  listClassName?: string
+}
+export const ProductsGroupList: React.FC<Props> = ({ title, items, className, categoryId, listClassName }) => {
+  return (
+    <div className={className}>
+      <Title text={title} size='lg' className='font-extrabold mb-5'/>
+      <div className="grid grid-cols-3 gap-[50px]">
+        {items.map((item, i) => (
+          <ProductCard
+            key={item.id}
+            id={item.id}
+            name="Маргарита"
+            imageUrl="https://media.dodostatic.net/image/r:292x292/11EE7D610BBEB562BD4D48786AD87270.webp"
+            price={390}
+            count={i % 2}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/shared/top-bar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Container} from "./container"
+import { cn } from '@/lib/utils';
+import { SortPopup } from "./sort-popup";
+import { Categories } from "./categories";
+
+interface Props {
+    className?: string;
+}
+
+
+
+export const TopBar: React.FC<Props> = ({className}) => {
+    return (
+        <div className={cn('sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10', className)}>
+        <Container className="flex items-center justify-between py-8">
+            <Categories/>
+            <SortPopup/>
+        </Container>
+       </div>
+        
+    )
+}
